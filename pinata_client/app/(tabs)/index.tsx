af4f9276f93c7a81aa336cd2c93400877d359bab
kim-seonwoo <sw204011@gmail.com>
2025-05-09 22:17:51 +0900

    set: iOS Complet login set up

--- pinata_client/app/(tabs)/index.tsx
+++ pinata_client/app/(tabs)/index.tsx
@@ -1,18 +1,55 @@
 import { StyleSheet } from "react-native";
-
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import auth from "@react-native-firebase/auth";
+import {
+  GoogleSignin,
+  isErrorWithCode,
+  isSuccessResponse,
+  statusCodes,
+  User,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
 
 export default function TabOneScreen() {
+  GoogleSignin.configure({
+    webClientId:
+      "921779819892-panh35g050pvvugg38snv788s0nnpiha.apps.googleusercontent.com",
+  });
+  const signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices({
+        showPlayServicesUpdateDialog: true,
+      });
+      const response = await GoogleSignin.signIn();
+      const googleCredential = auth;
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.IN_PROGRESS:
+            // operation (eg. sign in) already in progress
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            // Android only, play services not available or outdated
+            break;
+          default:
+          // some other error happened
+        }
+      } else {
+        // an error that's not related to google sign in occurred
+      }
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab 김선우 화이팅</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+      <GoogleSigninButton
+        style={{ width: 192, height: 48 }}
+        size={GoogleSigninButton.Size.Wide}
+        color={GoogleSigninButton.Color.Dark}
+        onPress={() => signIn()}
+        disabled={false}
       />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
@@ -33,3 +70,6 @@ const styles = StyleSheet.create({
     width: "80%",
   },
 });
+function setState(arg0: { userInfo: User }) {
+  throw new Error("Function not implemented.");
+}
