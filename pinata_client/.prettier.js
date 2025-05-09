ce7600af3417829092f2ba3bf2931e5cd1cad6af
kim-seonwoo <sw204011@gmail.com>
2025-05-09 15:36:57 +0900

    feat : init git

---
+++ pinata_client/.prettier.js
@@ -0,0 +1,12 @@
+// .prettierrc.js
+module.exports = {
+  singleQuote: true,
+  semi: true,
+  printWidth: 100,
+  useTabs: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "auto",
+};
