c3a8f8a7ed896d057955a6f6e7be70889fff77f8
kim-seonwoo <sw204011@gmail.com>
2025-05-09 21:19:59 +0900

    feat: gitignore 추가

--- pinata_client/app/(tabs)/index.tsx
+++ pinata_client/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { Text, View } from "@/components/Themed";
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab 김선우</Text>
+      <Text style={styles.title}>Tab 김선우 화이팅</Text>
       <View
         style={styles.separator}
         lightColor="#eee"
